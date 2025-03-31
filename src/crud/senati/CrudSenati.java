4413f68d3146c9dae221ced422cae8276170a811
mathias <1587786@senati.pe>
2025-03-31 09:06:30 -0500

    crear la clase conexion y su metodo conectar

--- src/crud/senati/CrudSenati.java
+++ src/crud/senati/CrudSenati.java
@@ -15,6 +15,10 @@ public class CrudSenati {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        Conexion cn= new Conexion();
+        
+        cn.conectar();
     }
     
 }
