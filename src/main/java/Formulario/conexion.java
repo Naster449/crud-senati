f2a2fce8e3b6a73078f63429a9e1af7464fdb4df
Naster449 <1587786@senati.pe>
2025-04-05 21:49:01 -0500

    conexion

---
+++ src/main/java/Formulario/conexion.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Formulario;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+/**
+ *
+ * @author Mathias
+ */
+public class conexion {
+    public Connection conectar(){
+        String url = "jdbc:mysql://localhost:3306/formulario?zeroDateTimeBehavior=CONVERT_TO_NULL";
+        String user = "root";
+        String password = "";
+        Connection cn = null;
+        
+        try{
+            cn =DriverManager.getConnection(url,user, password);
+            if (cn !=null){
+                System.out.println("Conectado");
+            }
+        }catch(SQLException e){
+            System.out.println(e);
+        }
+        return cn;
+    }
+    
+}
